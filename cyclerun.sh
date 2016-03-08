b6383c2995f74a24dc8a0e6f6c0222c085924589
Stefan Dierauf <sdierauf@gmail.com>
2016-03-07 16:40:03 -0800

    support cycle servers

--- cyclerun.sh
+++ cyclerun.sh
@@ -1,1 +1,1 @@
-THEANO_FLAGS=mode=FAST_RUN,device=cpu,openmp=true,floatX=float32 OMP_NUM_THREADS=96 python image_analogy.py images/mask.jpg images/mask.jpg images/face1.jpg out/face3
+THEANO_FLAGS=mode=FAST_RUN,device=cpu,openmp=true,floatX=float32 OMP_NUM_THREADS=96 python image_analogy.py images/mask.jpg images/mask.jpg images/face1.jpg bicycle/face3
